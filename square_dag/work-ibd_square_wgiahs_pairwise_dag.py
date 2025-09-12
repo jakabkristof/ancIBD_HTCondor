a32e4a739d0faffdb69ca0f9a0439651941bbc6a
jakabkristof <jakabkristof33@gmail.com>
2025-09-12 16:04:07 +0200

    reduced io operations

--- square_dag/work-ibd_square_wgiahs_pairwise_dag.py
+++ square_dag/work-ibd_square_wgiahs_pairwise_dag.py
@@ -3,16 +3,19 @@
 #arguments: iid_one iid_two in_folder out_folder
 #MOT2S185FQM MOTS95W - chr3&chr16 one segment, chr19 two segments
 
-
-
 import sys
 import os
-
-
 
 iid_one = sys.argv[1]
 
 iid_two = sys.argv[2]
+
+#
+
+if(iid_one == iid_two):
+    os._exit(0)
+
+#
 
 iid_pair = [iid_one, iid_two]
 
@@ -24,16 +27,6 @@ out_folder = sys.argv[4]
 
 prefix_out_iid_pair = '_'.join(iid_pair)
 
-chromosomes = range(1,23)
-
-#
-
-if(iid_one == iid_two):
-    os._exit(0)
-
-#if(iid_pair != sorted(iid_pair)):
-#    os._exit(0)
-
 #
 
 import pandas as pd
@@ -42,27 +35,23 @@ from ancIBD.run import hapBLOCK_chrom
 
 #
 
+ibd_segments_df = pd.DataFrame([], columns=["Start", "End", "StartM", "EndM", "length", "lengthM", "ch", "StartBP", "EndBP", "iid1", "iid2"])
+
+chromosomes = range(1,23)
+
 for chr in chromosomes:
-    hapBLOCK_chrom(folder_in=in_folder, iids=iid_pair,
-                   ch=chr, folder_out=out_folder,
-                   output=False, prefix_out=prefix_out_iid_pair, logfile=False, l_model='h5', e_model='haploid_gl2',
-                   h_model='FiveStateScaled', t_model='standard', ibd_in=1, ibd_out=10, ibd_jump=400,
+    segments = hapBLOCK_chrom(folder_in=in_folder, iids=iid_pair,
+                   ch=chr, folder_out='',
+                   output=False, logfile=False, l_model="h5", e_model="haploid_gl2",
+                   h_model="FiveStateScaled", t_model="standard", ibd_in=1, ibd_out=10, ibd_jump=400,
                    min_cm=6, cutoff_post=0.99, max_gap=0.0075)
+    ibd_segments_df = pd.concat([ibd_segments_df, segments[0]])
 
-#os.mkdir(out_folder + '/SEGMENTS_OUT/') 
 
-for chr in chromosomes:
-    input_file_path = out_folder + '/' + prefix_out_iid_pair + '/ch' + str(chr) + ".tsv"
-    line_count = open(input_file_path, 'r').read().count('\n')
-    if(line_count > 1):
-        ibd_df = pd.read_csv(input_file_path, sep='\t')
-        ibd_df.iid1 = iid_one
-        ibd_df.iid2 = iid_two
-        ibd_df.drop("StartBP", axis=1, inplace=True)
-        ibd_df.drop("EndBP", axis=1, inplace=True)
-        ibd_df.to_csv(out_folder+"/SEGMENTS_OUT.tsv", mode='a', index=False, header=False, sep='\t')
-        #ibd_df.to_csv(out_folder+'/SEGMENTS_OUT/'+prefix_out_iid_pair+'_chr' + str(chr) + ".tsv", index=False, sep='\t')
-    os.remove(input_file_path) 
-
-os.rmdir(out_folder + '/' + prefix_out_iid_pair)
+if(len(ibd_segments_df) > 0):
+    ibd_segments_df.iid1 = iid_one
+    ibd_segments_df.iid2 = iid_two
+    ibd_segments_df.drop("StartBP", axis=1, inplace=True)
+    ibd_segments_df.drop("EndBP", axis=1, inplace=True)
+    ibd_segments_df.to_csv(out_folder+"/SEGMENTS_OUT.tsv", mode='a', index=False, header=False, sep='\t')
 
